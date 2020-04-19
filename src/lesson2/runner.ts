c0a991ada42ea62d0c94131e2e608ed6d6db3954
alexkhait <alexkhait@yandex-team.ru>
2020-04-19 16:54:10 +0300

    lesson-2-calc: убрала лишнюю перепроверку в ранере

--- src/lesson2/runner.ts
+++ src/lesson2/runner.ts
@@ -17,29 +17,13 @@ export const runner = (line: string): number => {
 
   const unaryPrefixPrioritiesRes = unaryPrefixPrioritiesCalc(stack);
 
-  if (unaryPrefixPrioritiesRes.length === 1) {
-    return Number(unaryPrefixPrioritiesRes[0]);
-  }
-
   const unaryPostfixPrioritiesRes = unaryPostfixPrioritiesCalc(
     unaryPrefixPrioritiesRes
   );
 
-  if (unaryPostfixPrioritiesRes.length === 1) {
-    return Number(unaryPostfixPrioritiesRes[0]);
-  }
-
   const zerothPrioritiesRes = zerothPrioritiesCalc(unaryPostfixPrioritiesRes);
 
-  if (zerothPrioritiesRes.length === 1) {
-    return Number(zerothPrioritiesRes[0]);
-  }
-
   const firstPrioritiesRes = firstPrioritiesCalc(zerothPrioritiesRes);
-
-  if (firstPrioritiesRes.length === 1) {
-    return Number(firstPrioritiesRes[0]);
-  }
 
   return secondPrioritiesCalc(firstPrioritiesRes);
 };
